ded317ac26c58cfb10964a581b081a17c06d6a46
Santiago Hernandez <trollearvpn156@gmail.com>
2026-04-28 00:11:32 -0500

    final commit with url to supabase

--- app/database.py
+++ app/database.py
@@ -6,7 +6,8 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 env_path = BASE_DIR / ".env"
-load_dotenv(dotenv_path=env_path)
+if os.getenv("ENV") != "production":
+    load_dotenv(dotenv_path=env_path)
 
 from dotenv import dotenv_values
 config = dotenv_values(env_path)
